c417bbfb309c6995704360465e2f8b3565844513
Alonso S <alonsosalcido13@gmail.com>
2016-09-08 02:31:13 -0600

    ButterKnife, injection of view, firstpart of the calculator.

---
+++ app/src/main/java/edu/tecii/android/tipcalc01/MainActivity.java
@@ -0,0 +1,128 @@
+package edu.tecii.android.tipcalc01;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.inputBill)
+    EditText inputBill;
+    @Bind(R.id.btnSubmit)
+    Button btnSubmit;
+    @Bind(R.id.inputPercentage)
+    EditText inputPercentage;
+    @Bind(R.id.btnIncrease)
+    Button btnIncrease;
+    @Bind(R.id.btnDecrease)
+    Button btnDecrease;
+    @Bind(R.id.btnClear)
+    Button btnClear;
+    @Bind(R.id.txtTip)
+    TextView txtTip;
+    //EditText inputBill;
+
+    private final static int TIP_STEP_CHANGE = 1;
+    private  final static int DEFAULT_TIP_PERCENTAGE = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        //inputBill = (EditText) findViewById(R.id.inputBill);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_about) {
+            about();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.btnSubmit)
+    public void handleClickSubmit(){
+        hideKeyboard();
+        String strInputTotal = inputBill.getText().toString().trim();
+        if(!strInputTotal.isEmpty()){
+            double total = Double.parseDouble(strInputTotal);
+            int tipPercentage = getTipPercentage();
+            double tip = total*(tipPercentage/100d);
+
+            String strTip = String.format(getString(R.string.global_message_tip), tip);
+            txtTip.setVisibility(View.VISIBLE);
+            txtTip.setText(strTip);
+        }
+    }
+
+    @OnClick(R.id.btnIncrease)
+    public void handleClickIncrease(){
+        hideKeyboard();
+        handleTipChange(TIP_STEP_CHANGE);
+    }
+
+    @OnClick(R.id.btnDecrease)
+    public void handleClickDecrease(){
+        hideKeyboard();
+        handleTipChange(-TIP_STEP_CHANGE);
+    }
+
+    private void handleTipChange(int change) {
+        int tipPercentage = getTipPercentage();
+        tipPercentage += change;
+        if(tipPercentage > 0){
+            inputPercentage.setText(String.valueOf(tipPercentage));
+        }
+    }
+
+    public int getTipPercentage(){
+        int tipPercentage = DEFAULT_TIP_PERCENTAGE;
+        String strInputTipPercentage = inputPercentage.getText().toString().trim();
+        if(!strInputTipPercentage.isEmpty()){
+            tipPercentage = Integer.parseInt(strInputTipPercentage);
+        }else{
+            inputPercentage.setText(String.valueOf(tipPercentage));
+        }
+        return tipPercentage;
+    }
+
+    private void hideKeyboard(){
+        InputMethodManager inputManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        try {
+            inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }catch (NullPointerException npe){
+            Log.e(getLocalClassName(), Log.getStackTraceString(npe));
+        }
+    }
+
+    private void about() {
+        TipCalcApp app = (TipCalcApp) getApplication();
+        String strUrl = app.getAboutUrl();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(strUrl));
+        startActivity(intent);
+    }
+}
